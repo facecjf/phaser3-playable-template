3d449b79299259b12f0176f9bae9d577c094cfad
facecjf <facecjf@gmail.com>
2025-09-20 10:54:20 -0500

    Update MainSceneSC.js

--- src/js/MainSceneSC.js
+++ src/js/MainSceneSC.js
@@ -242,7 +242,7 @@ export default class MainScene extends Phaser.Scene {
         //     .setTint(0xFFFFFF);
 
         //Add web font text to tutBG
-        this.tutText = this.add.text(this.centerX, tutY, this.getLocalizedText('game_tut'), { fontFamily: 'speechFont', fontSize: '32px', color: '#096a55' })
+        this.tutText = this.add.text(this.centerX, tutY, this.getLocalizedText('game_tut'), { fontFamily: 'speechFont', fontSize: '48px', color: '#FFFFFF' })
             .setDepth(11)
             .setOrigin(0.5)
             .setTint(0xFFFFFF);
